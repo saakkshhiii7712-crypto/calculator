9eb61001ce06662c2764961793023dc56bd615c5
Sakshi <saakkshhiii7712@gmail.com>
2026-03-24 22:24:55 +0530

    Update divide.py

--- divide.py
+++ divide.py
@@ -1,2 +1,8 @@
 def divide(a, b):
+    if b == 0:
+        return "Division by zero is not possible"
     return a / b
+
+def display_divide(a, b):
+    result = divide(a, b)
+    print("Division =", result)
